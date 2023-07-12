edee4ae60e656e3a53c74e67c3fb0821b32b42cf
LucaPatarca <luca.patarca@gmail.com>
2023-07-12 13:25:59 +0200

    Fix font2

--- scripts/post/nerd-fonts.sh
+++ scripts/post/nerd-fonts.sh
@@ -2,8 +2,8 @@
 
 set -oue pipefail
 
-mkdir -p /usr/local/share/fonts/
-cd /usr/local/share/fonts/
+mkdir -p /usr/share/fonts/nerd-fonts
+cd /usr/share/fonts/nerd-fonts
 curl -fLO https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/FiraMono.zip
 unzip FiraMono.zip
 rm FiraMono.zip
