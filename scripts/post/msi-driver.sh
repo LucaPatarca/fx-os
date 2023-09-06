4f60e624d11794d7027f5fb3a6c50abd214e8558
Luca Patarca <luca.patarca@gmail.com>
2023-09-06 11:25:32 +0200

    update msi-driver

--- scripts/post/msi-driver.sh
+++ scripts/post/msi-driver.sh
@@ -9,5 +9,5 @@ echo "KERNEL_VERSION=$KERNEL_VERSION"
 git clone https://github.com/BeardOverflow/msi-ec.git
 cd msi-ec
 make -C /lib/modules/$KERNEL_VERSION/build M=$(pwd) modules
-cp /tmp/scripts/msi-ec.ko /lib/modules/$KERNEL_VERSION/extra/
+cp msi-ec.ko /lib/modules/$KERNEL_VERSION/extra/
 depmod $KERNEL_VERSION
