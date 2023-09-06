26bffc05df467a84ad55885b243e91582877aa38
Luca Patarca <luca.patarca@gmail.com>
2023-09-06 11:05:54 +0200

    update msi

--- scripts/post/msi-copy.sh
+++ scripts/post/msi-copy.sh
@@ -6,5 +6,8 @@ KERNEL_VERSION=$(ls /lib/modules/ | sort | tail -n 1)
 
 echo "KERNEL_VERSION=$KERNEL_VERSION"
 
+git clone https://github.com/BeardOverflow/msi-ec.git
+cd msi-ec
+make -C /lib/modules/$KERNEL_VERSION/build M=$(pwd) modules
 cp /tmp/scripts/msi-ec.ko /lib/modules/$KERNEL_VERSION/extra/
 depmod $KERNEL_VERSION
