35acc95bd342ab4ab6d6ab02602f419c2f31c146
Luca Patarca <luca.patarca@gmail.com>
2023-09-06 12:40:16 +0200

    Fix msi-driver

--- scripts/post/msi-driver.sh
+++ scripts/post/msi-driver.sh
@@ -8,6 +8,7 @@ echo "KERNEL_VERSION=$KERNEL_VERSION"
 
 git clone https://github.com/BeardOverflow/msi-ec.git
 cd msi-ec
+ln -s /usr/bin/ld.bfd /usr/bin/ld
 make -C /lib/modules/$KERNEL_VERSION/build M=$(pwd) modules
 cp msi-ec.ko /lib/modules/$KERNEL_VERSION/extra/
 depmod $KERNEL_VERSION
